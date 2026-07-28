57aa1e6eec007e4b032c43552e87be5f4dcaab32
Anirudh <anirudh614345@gmail.com>
2026-07-28 11:50:00 +0530

    Add remaining couture products to Canva sync

--- api/canva/_sync-manifest.js
+++ api/canva/_sync-manifest.js
@@ -68,6 +68,37 @@ const createProductEntries = ({
     ),
   );
 
+const READY_PRODUCT_ROLES = Object.freeze([
+  "hero",
+  "front",
+  "side",
+  "back",
+  "detail",
+  "drape",
+  "editorial",
+]);
+
+const createReadyProductEntries = ({
+  productSlug,
+  productName,
+  canvaPageIds,
+}) => {
+  if (canvaPageIds.length !== READY_PRODUCT_ROLES.length) {
+    throw new Error("Ready Canva products must contain seven assets.");
+  }
+
+  return createProductEntries({
+    productSlug,
+    productName,
+    assetFolder: `Rangbastra/Products/${productName}`,
+    bindings: canvaPageIds.map((canvaPageId, index) => [
+      canvaPageId,
+      `${productName}_Web_${String(index + 1).padStart(2, "0")}`,
+      READY_PRODUCT_ROLES[index],
+    ]),
+  });
+};
+
 export const CANVA_SYNC_MANIFEST = Object.freeze(
   validateCanvaSyncManifest([
     Object.freeze({
@@ -217,6 +248,253 @@ export const CANVA_SYNC_MANIFEST = Object.freeze(
         ["PBxQRjqT6X17qLxm", "Ayana_Web_05", "detail"],
         ["PBQnKdXKqvHsSGTd", "Ayana_Web_06", "drape"],
         ["PBHV5QnL9YjCC9B0", "Ayana_Web_07", "editorial"],
+      ],
+    }),
+    ...createReadyProductEntries({
+      productSlug: "chaarvi",
+      productName: "Chaarvi",
+      canvaPageIds: [
+        "PBjc4j56VmXLdTzM",
+        "PBJ6scSgD0BTCvdC",
+        "PBH24yR9FYGRxhzs",
+        "PBpmNR41LJGmHGJd",
+        "PB2f0Pz2lNDJkV34",
+        "PBmv68040Qrtthpj",
+        "PB5LTGC7c20vlyQB",
+      ],
+    }),
+    ...createReadyProductEntries({
+      productSlug: "tiara",
+      productName: "Tiara",
+      canvaPageIds: [
+        "PBHr0YTyDTdgyQwG",
+        "PBHXN358xszyRqNq",
+        "PB56DYjTGmQfMvGv",
+        "PBn2pXZqy2Mqs5qx",
+        "PB3wtmsv36d7n8J0",
+        "PB7V4sqZvmsdWXYV",
+        "PBqwxVG20xxBTGSq",
+      ],
+    }),
+    ...createReadyProductEntries({
+      productSlug: "bahaar",
+      productName: "Bahaar",
+      canvaPageIds: [
+        "PBrHc0mb7zw351PT",
+        "PBgZ11r9vsRld2Q2",
+        "PB8WV3wL9R03wFtt",
+        "PBdrWm9tnyNGMqbG",
+        "PB3WlHn1qsrFFm4D",
+        "PBtKbTfQlTR4F7m4",
+        "PBcRvj8TRcfcZscy",
+      ],
+    }),
+    ...createReadyProductEntries({
+      productSlug: "aarini",
+      productName: "Aarini",
+      canvaPageIds: [
+        "PBKclzBB41gkd1ZF",
+        "PB3GN1bl8FzhxZsX",
+        "PBS1cb7mBLQqcGrP",
+        "PBhtp36ncwlvjCl7",
+        "PBbzKR27NcSsdhHC",
+        "PBJFcY4FBWKq1lxm",
+        "PBwyhcrQsz34M5vm",
+      ],
+    }),
+    ...createReadyProductEntries({
+      productSlug: "nirvi",
+      productName: "Nirvi",
+      canvaPageIds: [
+        "PB2wKCPvyZLSPWnd",
+        "PBTj1lCddTkgf0gJ",
+        "PBxSQrnkxXFycpfL",
+        "PBk7rksWkYVkwtww",
+        "PBPth3kZTyfslzvf",
+        "PBzl4G3V019D74c2",
+        "PBF3hZLZ2vnXWs70",
+      ],
+    }),
+    ...createReadyProductEntries({
+      productSlug: "sahira",
+      productName: "Sahira",
+      canvaPageIds: [
+        "PBV2dhPYJPX05vfW",
+        "PBlbJy645jlwfTjj",
+        "PBgJjdYGlK1m6Nh1",
+        "PB9W7Cx02Jkybhhx",
+        "PB0NHTMPvWYfsN56",
+        "PBYhzBgqhLyylrfC",
+        "PBMkP9LY2gNnpFy4",
+      ],
+    }),
+    ...createReadyProductEntries({
+      productSlug: "zavira",
+      productName: "Zavira",
+      canvaPageIds: [
+        "PBFcCQ4HvNTFjkMn",
+        "PBHPyNwsYlrrlnNM",
+        "PBT62m66NBLbsPZJ",
+        "PB0S0Tr2WfSnJ59f",
+        "PBpPdHgwHNKpLLqf",
+        "PBFYvGl0Xp8SvFyz",
+        "PBknfQ4H3j4zBQbM",
+      ],
+    }),
+    ...createReadyProductEntries({
+      productSlug: "aureya",
+      productName: "Aureya",
+      canvaPageIds: [
+        "PBzKl01FngdbGRFC",
+        "PBTBVybLQmz28GGX",
+        "PBXnMVY4fPHjWVpX",
+        "PBjQbWyf8L8cDQkJ",
+        "PBVX8gWrthlK6ZBV",
+        "PByQS1fKHp8XTY5c",
+        "PBMKsx6yNMqVkyMz",
+      ],
+    }),
+    ...createReadyProductEntries({
+      productSlug: "mishka",
+      productName: "Mishka",
+      canvaPageIds: [
+        "PBPqh1T5PX9FY4YC",
+        "PB3HfRNLs213DxnK",
+        "PBXq9vZmFLQhFbcf",
+        "PB3R2GHyFWXJw7CJ",
+        "PBn2qcYx863lcV32",
+        "PBBHt0WQR0Q1Cl7F",
+        "PB5q57FSyJWRVFf7",
+      ],
+    }),
+    ...createReadyProductEntries({
+      productSlug: "varnika",
+      productName: "Varnika",
+      canvaPageIds: [
+        "PBKLjnDcwJ3hRfXY",
+        "PBZNCDJ7Zj3k5P3f",
+        "PB0W6T7z2vSwg9DY",
+        "PBqzBNJqBBss019n",
+        "PBmLFt6yhB7cPm40",
+        "PBnRzh2RnC1WrCDJ",
+        "PB432G3J8wgQGnQW",
+      ],
+    }),
+    ...createReadyProductEntries({
+      productSlug: "lavanya",
+      productName: "Lavanya",
+      canvaPageIds: [
+        "PBmgXq1SnVHbv9Nc",
+        "PB1rlH7Vz8dvXbm6",
+        "PBKnYHTygl1ZWnNY",
+        "PBs8j1DkQNvBWfcL",
+        "PB7Z5nKQK5BFjWpq",
+        "PBh6y5vT8Swh3DrH",
+        "PBvn8dKzX5mp2yHm",
+      ],
+    }),
+    ...createReadyProductEntries({
+      productSlug: "ruhaaya",
+      productName: "Ruhaaya",
+      canvaPageIds: [
+        "PBXCyXGPZjxyr2kH",
+        "PBx1PNrMz61sMDMv",
+        "PBtScvNbhCRFqkJx",
+        "PB40BpgjB8XlQFY5",
+        "PB8TDmMLcbgCDD3l",
+        "PBMnHMsrwM0Phzj3",
+        "PBQ0zVcGjfRM7sNX",
+      ],
+    }),
+    ...createReadyProductEntries({
+      productSlug: "eshaira",
+      productName: "Eshaira",
+      canvaPageIds: [
+        "PBYZ4TmCjpz8DlV6",
+        "PBZsKq36rJ5qShMG",
+        "PBWGX2VGlbKRxPlV",
+        "PBxncnY0QGM00sJX",
+        "PB82lGgGcWHZy95g",
+        "PBJnsDHT6ppPlhVD",
+        "PBhPGYsXqQNhJYkf",
+      ],
+    }),
+    ...createReadyProductEntries({
+      productSlug: "eiraa",
+      productName: "Eiraa",
+      canvaPageIds: [
+        "PBmtXL6BPg9KR5cV",
+        "PB2XDSyFBmqLR55h",
+        "PBgFZbtrqLsMFCRt",
+        "PBFg8SdClQjBdfSg",
+        "PB8Jb2ZJlQ3VClc8",
+        "PBZsgD6qt0Fql4zn",
+        "PBfQ9Bn2kBmCM4N5",
+      ],
+    }),
+    ...createReadyProductEntries({
+      productSlug: "ziana",
+      productName: "Ziana",
+      canvaPageIds: [
+        "PBZR9HSNKJjTFwB1",
+        "PBmPXMWzbBl6SwnC",
+        "PBMJqffLWTJwXxKX",
+        "PBVjbhd27BbPt6R8",
+        "PB0VH7mBP6N1vjdQ",
+        "PBBSXj2NHjr7XdFy",
+        "PBxPfkTxK5YbqBmB",
+      ],
+    }),
+    ...createReadyProductEntries({
+      productSlug: "aavya",
+      productName: "Aavya",
+      canvaPageIds: [
+        "PBJBVMpTGbS5bpX7",
+        "PBG6j5HFmmtgqpBP",
+        "PBq3FfQjLRYTql1f",
+        "PBY1tZtjKlBb5Hmn",
+        "PBXtWpNfQdvpzdps",
+        "PBK0nnvZgSWqrFrp",
+        "PBlzycly2tHj79H2",
+      ],
+    }),
+    ...createReadyProductEntries({
+      productSlug: "anaahita",
+      productName: "Anaahita",
+      canvaPageIds: [
+        "PB1rmKcw42tLxZR7",
+        "PBBKkbV5ZnmtQcv0",
+        "PBGj9ngS5y5R3hTP",
+        "PBVKL7TtPCf4BKl7",
+        "PBSlvysKWTRT9dzN",
+        "PB1TFVmGKvG6VvVZ",
+        "PBV3ZGXnqgBZ0lgW",
+      ],
+    }),
+    ...createReadyProductEntries({
+      productSlug: "riva",
+      productName: "Riva",
+      canvaPageIds: [
+        "PBDxwG2h1MLMSr7k",
+        "PBng8cMmTNkzCdWJ",
+        "PBlcPJmykYdqnD3n",
+        "PBYnJS3W1TFLlRMv",
+        "PBCLzzFmfJkdnMX0",
+        "PBhcKGbd9FsDmjF4",
+        "PBccryzFzBgZBXql",
+      ],
+    }),
+    ...createReadyProductEntries({
+      productSlug: "iraaya",
+      productName: "Iraaya",
+      canvaPageIds: [
+        "PB4sgLC5FRsybSYM",
+        "PBbLRzsDvJ9qjhwm",
+        "PB8jM67FKlYHbLcQ",
+        "PB6BbC2fqyQ0GCWp",
+        "PBFw8V4L85g8XKtF",
+        "PBqHkyxTqLm5yhtG",
+        "PBsCRFlZyYtZnSYl",
       ],
     }),
   ]),
